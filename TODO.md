d918b5d3e0c7dfff05fdc031b66874d8a859d4bb
Ian Kropp <iankropp@periphery.periphery>
2016-08-31 23:09:40 -0400

    Made some bug tweaks to error handling in setup.rb

---
+++ TODO.md
@@ -0,0 +1,10 @@
+# Future features
+
+1. Cleaner error handling
+
+2. Set zsh as the shell automatically
+
+3. Vundle install (if possible from commandline)
+
+4. Automatically install zsh and curl depending on the OS
+
--- setup.rb
+++ setup.rb
@@ -10,19 +10,28 @@ end
 
 
 puts 'Copying .vim directory...'
-`cp -iR vim ~/.vim`
+`cp -R vim ~/.vim`
 fail "Failed to deploy .vim directory" if not $?.success?
 
 puts 'Success! Copying .vimrc directory...'
-`cp -i vimrc ~/.vimrc`
+`cp  vimrc ~/.vimrc`
 fail "Failed to deploy .vimrc" if not $?.success?
 
+puts 'Success! Looking for curl...'
+`curl --version`
+fail "curl not installed. Install curl before continuing" if not $?.success?
+
+puts 'Success! Looking for zsh...'
+blah=`zsh --version`
+fail "zsh not installed. Install zsh before continuing" if not $?.success?
+
 puts 'Success! Downloading Oh My Zsh...'
-`sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"`
+blah=`sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"`
+print blah
 fail "Failed to download Oh My Zsh." if not $?.success?
 
 puts 'Success! Copying zshrc...'
-`cp -i zshrc ~/.zshrc`
+`cp zshrc ~/.zshrc`
 fail "Failed to deploy zshrc" if not $?.success?
 
 puts 'Over all success! Happy hacking!'
