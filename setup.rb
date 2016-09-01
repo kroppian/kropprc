9988997e6909128340af8bb7ca94d348c67b5bd2
Ian Kropp <ian.kropp@mightyinthemidwest.com>
2016-08-31 23:34:10 -0400

    Adding tmux.conf to the setup

--- setup.rb
+++ setup.rb
@@ -11,8 +11,6 @@ puts 'Checking for vim...'
 `vim --version`
 fail 'vim not installed. Install and start the setup again' if not $?.success?
 
-
-
 puts 'Creating the .vim directory...'
 `mkdir -p ~/.vim/bundle`
 fail "Failed to create the .vim directory" if not $?.success?
@@ -21,26 +19,31 @@ puts 'Attempting to clone Vundle...'
 `git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim`
 fail "Failed to clone Vundle." if not $?.success?
 
-puts ' Copying .vimrc directory...'
+puts 'Copying .vimrc directory...'
 `cp  vimrc ~/.vimrc`
 fail "Failed to deploy .vimrc" if not $?.success?
 
-puts 'Success! Looking for curl...'
+puts 'Looking for curl...'
 `curl --version`
 fail "curl not installed. Install curl before continuing" if not $?.success?
 
-puts 'Success! Looking for zsh...'
+puts 'Looking for zsh...'
 blah=`zsh --version`
 fail "zsh not installed. Install zsh before continuing" if not $?.success?
 
-puts 'Success! Downloading Oh My Zsh...'
+puts 'Downloading Oh My Zsh...'
 blah=`sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"`
 print blah
 fail "Failed to download Oh My Zsh." if not $?.success?
 
-puts 'Success! Copying zshrc...'
+puts 'Copying zshrc...'
 `cp zshrc ~/.zshrc`
 fail "Failed to deploy zshrc" if not $?.success?
+
+puts 'Copying tmux.conf...'
+`cp tmux.conf ~/.tmux.conf`
+fail "Failed to deploy tmux.conf" if not $?.success?
+
 
 puts 'Over all success! Happy hacking!'
 
